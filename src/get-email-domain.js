521f50385b070de5a98e41cc7041b8a36132c32f
Aleks <andriushyn.aleks@gmail.com>
2022-10-17 19:00:45 +0300

    feat: g-e-d-2

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,7 +10,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
+function getEmailDomain(email) {
   const domain = email.split("@");
   return domain[1];
 }
